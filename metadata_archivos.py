8c52617cbcb4bce9b390a3c8be5b0108833135f8
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-03-19 14:21:53 -0300

    Agregando info de como deberia ser la metadata

--- metadata_archivos.py
+++ metadata_archivos.py
@@ -28,12 +28,42 @@ def filtrar(archivo, config):
             return True
     return False
 
-def procesarArchivo(index, nombreArchivo, directorio):
+def procesarArchivo(nombreArchivo, outgoingLinks):
+    # Todos la metadata del archivo como keys en el diccionario
+    # tags: [] -> lista de tags de la pagina
+    # file: 
+    #   * cday -> ctime pero sin contar los milisegundos, segundos, minutos ni horas
+    #   * ctime -> which refers to the last time some metadata related to the file was changed.
+    #   * etags -> tags
+    #   * ext -> "md"
+    #   * folder -> absoluta
+    #   * frontmatter -> repetir lo mismo que antes, metadata y tags
+    #   * inlinks
+    #   * link
+    #       * display -> nombre
+    #       * embed -> True / False
+    #       * path
+    #       * subpath
+    #       * type -> file
+    #   * mday -> mtime pero sin contar los milisegundos, segundos, minutos ni horas
+    #   * mtime -> which is the last time a file’s contents were modified.
+    #   * name
+    #   * outlinks -> lo generamos como una lista vacia inialmente
+    #       * display -> nombre
+    #       * embed -> True / False
+    #       * path
+    #       * subpath
+    #       * type -> file
+    #   * path -> folder + name + . + ext
+    #   * size
+    #   * tags -> las mismas q etags (la tag es #nombre)
+    #   * tasks 
     return {
-        "index": index,
         "nombre": nombreArchivo.replace(directorio, ""),
     }
 
+def encontrarArchivo(metadata):
+    pass
 
 def main(argv):
     if len(argv) <= 1:
@@ -51,18 +81,38 @@ def main(argv):
 
     directorio = argv[1]
     allFiles = argv[2]
-    generador = GenArchivos(directorio)
 
     metadata = {
         'files': [],
     }
 
-    for index, archivo in enumerate(generador):
+    outgoingLinks = {}
+
+    generador = GenArchivos(directorio)
+    for archivo in generador:
         if filtrar(archivo, config):
             continue
 
-        metadataArchivo = procesarArchivo(index, archivo, directorio)
+        archivo = archivo.replace(directorio, "")
+
+        metadataArchivo = procesarArchivo(archivo, outgoingLinks)
         metadata["files"].append(metadataArchivo)
+
+    generador = GenArchivos(directorio)
+    for archivo in generador:
+        if filtrar(archivo, config):
+            continue
+
+        archivo = archivo.replace(directorio, "")
+        if not archivo in outgoingLinks:
+            continue
+
+        pos = encontrarArchivo(metadata)
+        if pos < 0:
+            continue
+        
+        metadata["files"][pos]["file"]["outlinks"].append(outgoingLinks[archivo])
+
 
     with open(allFiles, "w", encoding = "ISO-8859-1") as metadataFile:
         json.dump(metadata, metadataFile, ensure_ascii=False)
