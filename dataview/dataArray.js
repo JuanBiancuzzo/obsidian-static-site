fa0a57c5fa84a85e59f664b43b619883bf69daef
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2024-03-22 08:34:51 -0300

    Limpeando dataArray

--- dataview/dataArray.js
+++ dataview/dataArray.js
@@ -1,48 +1,50 @@
 
 export const dataArrayProxyHandler = {
 
-  get: function(target, prop, receiver) {
-    if (prop === 'sort')
+    get: function(target, prop, receiver) {
+        if (prop === 'sort')
         return (compareFunction) => dataArraySort(target, compareFunction);
 
-    if (prop === 'groupBy') 
+        if (prop === 'groupBy') 
         return (keyGetter) => dataArrayGroupBy(target, keyGetter);
 
-    if (prop === 'distinct')
+        if (prop === 'distinct')
         return () => dataArrayDistinct(target);
 
-    if (prop === 'where') 
+        if (prop === 'where') 
         return (predicate) => dataArrayWhere(target, predicate);
-    
-    return Reflect.get(...arguments);
-  }
+
+        return Reflect.get(...arguments);
+    }
 
 };
 
 // Sort function
 function dataArraySort(array, compareFunction) {
-  return [...array].sort(compareFunction);
+    return [...array].sort(compareFunction);
 }
 
 // GroupBy function
 function dataArrayGroupBy(array, keyGetter) {
-  const grouped = new Map();
-  array.forEach(item => {
-    const key = keyGetter(item);
-    const collection = grouped.get(key) || [];
-    collection.push(item);
-    grouped.set(key, collection);
-  });
-  return grouped;
+    const grouped = new Map();
+
+    array.forEach(item => {
+        const key = keyGetter(item);
+        const collection = grouped.get(key) || [];
+        collection.push(item);
+        grouped.set(key, collection);
+    });
+
+    return grouped;
 }
 
 // Distinct function
 function dataArrayDistinct(array) {
-  return Array.from(new Set(array));
+    return Array.from(new Set(array));
 }
 
 // Where function
 function dataArrayWhere(array, predicate) {
-  return array.filter(predicate);
+    return array.filter(predicate);
 }
 
